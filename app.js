a709c5809e83a5109e25cfde84a002d78ba8236c
naveen3310 <73732120+naveen3310@users.noreply.github.com>
2021-07-03 13:30:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ function mypics() {
   var randomImg2 = Math.floor(Math.random() * 5) + 1;
   var url = "images/gima/" + randomImg + ".jpg";
   var url2 = "images/somenudes/" + randomImg2 + ".png";
-  var url3 = "images/mithi/1.jpg";
+  var url3 = "http://tamilactressesnavel.yolasite.com/resources/Item-Ranjana1.jpg";
   $(document).ready(function () {
     $(`<img src='${url}'>`).appendTo("#container");
     $(`<img src='${url2}'>`).appendTo("#container2");
