a62e42bb6321ad5ea0dd46056adcbf5756e2200d
naveen3310 <73732120+naveen3310@users.noreply.github.com>
2021-07-09 16:21:33 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 function mypics() {
+  var newArray = [
+    "https://infosaurs.com/images/blog_master/gima-ashi/gima%20ashi.jpg",
+  ];
+  var pusharray = $(".textareaa").val();
+
+  newArray.push(pusharray);
+  var valueofimg = newArray[Math.floor(Math.random() * newArray.length)];
+
   var randomImg = Math.floor(Math.random() * 21) + 1;
-  var randomImg2 = Math.floor(Math.random() * 5) + 1;
+
   var url = "images/gima/" + randomImg + ".jpg";
-  var url2 = "images/somenudes/" + randomImg2 + ".png";
-  var url3 = "http://tamilactressesnavel.yolasite.com/resources/Item-Ranjana1.jpg";
+
   $(document).ready(function () {
     $(`<img src='${url}'>`).appendTo("#container");
-    $(`<img src='${url2}'>`).appendTo("#container2");
-    $(`<img src='${url3}'>`).appendTo("#container3");
+
+    $(`<img src='${valueofimg}'>`).appendTo("#container3");
   });
 }
-function mypics2() {
-
-  var newArray =[]; 
-var pusharray=$(.storetext).val();
-newArray.push(pusharray); 
-  $("<img src="${newArray[0]}">").appendTo("container4");
-  }
